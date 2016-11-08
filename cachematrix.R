5f2874832e0bf16a2867c1f903940d011aee6f3b
lmyqnjz <2930414612@qq.com>
2016-11-08 12:58:26 +0800

    Programming Assignment2 second version

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,7 @@
+## This function creates a special "matrix" object that can cache its inverse
+## Creates an environment for the matrix 
+## solves the matrix or chaches the solution (if it was solved previously)
+
 makeCacheMatrix = function(mat) {
   inverse.mat = solve(mat)
   x = list(matrix = mat,
@@ -6,6 +10,12 @@ makeCacheMatrix = function(mat) {
   x
 }
 
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
+## Otherwise, it calculates the inverse of the data
+## and sets the value of the inverse in the cache via the setmean function.
 
 cacheSolve = function(obj){
   if(class(obj) == "cache.inverse.matrix")
